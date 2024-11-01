b571006f330e7ace5aa8c942b9bfde1a7750529b
Yazan Mtawej <yznmtwj@gmail.com>
2024-11-01 16:24:13 +0300

    add add exercise bottom sheet and build add exercise form

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:exercise_application/Models/exercise_model.dart';
 import 'package:exercise_application/constants.dart';
+import 'package:exercise_application/cubit/exercise_cubit/exercise_cubit.dart';
 import 'package:exercise_application/views/exercise_view.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-
-void main() async{
-  //Bloc.observer = SimpleBlocObserver();
+void main() async {
+ // Bloc.observer = SimpleBlocObserver;
   await Hive.initFlutter();
   Hive.registerAdapter(ExerciseModelAdapter());
   await Hive.openBox<ExerciseModel>(kExerciseBox);
 
-  runApp(DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const ExerciseApp()), // Wrap your app
-    );
+  runApp(
+    DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => const ExerciseApp()), // Wrap your app
+  );
 }
 
 class ExerciseApp extends StatelessWidget {
@@ -24,10 +26,13 @@ class ExerciseApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark),
-      home: const ExersiseView(),
+    return BlocProvider(
+      create: (context) => ExerciseCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark),
+        home: const ExersiseView(),
+      ),
     );
   }
 }
--- lib/widgets/add_exercise_bottom_sheet.dart
+++ lib/widgets/add_exercise_bottom_sheet.dart
@@ -1,11 +1,46 @@
 
+import 'package:exercise_application/cubit/add_exercise_cubit/add_exercise_cubit.dart';
+import 'package:exercise_application/cubit/exercise_cubit/exercise_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddExerciseBottomSheet extends StatelessWidget {
   const AddExerciseBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create:(context) => AddExerciseCubit(),
+      child: BlocConsumer<AddExerciseCubit,AddExerciseState>(listener: (context,state){
+         if (state is AddExerciseFailure) {}
+
+        if (state is AddExerciseSuccess) {
+          BlocProvider.of<ExerciseCubit>(context).fetchAlExercise();
+          Navigator.pop(context);
+        }
+      },
+      builder: (context,state){
+       return AbsorbPointer(
+          absorbing: state is AddExerciseLoding ? true : false,
+          child: Padding(
+            padding: EdgeInsets.only(
+                left: 16,
+                right: 16,
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: const SingleChildScrollView(child: AddExerciseForm()),
+          ),
+        );
+      } ),
+      
+      );
+  }
+}
+
+class AddExerciseForm extends StatelessWidget {
+  const AddExerciseForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
