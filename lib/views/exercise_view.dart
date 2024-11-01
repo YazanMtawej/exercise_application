ea31603f221a3eaaaf88c2c1b9a652c5de6fcf48
Yazan Mtawej <yznmtwj@gmail.com>
2024-11-01 15:02:14 +0300

    build exercise view

---
+++ lib/views/exercise_view.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class ExersiseView extends StatelessWidget {
+  const ExersiseView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container();
+  }
+}
