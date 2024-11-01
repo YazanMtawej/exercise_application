e3f8163b2f6bc955df0a6c9741b8124123de90c5
Yazan Mtawej <yznmtwj@gmail.com>
2024-11-01 15:14:21 +0300

    build Add Exercise Bottom Sheet and  Exercise View Body in Exersise View

--- lib/views/exercise_view.dart
+++ lib/views/exercise_view.dart
@@ -6,6 +6,35 @@ class ExersiseView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return  Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        showModalBottomSheet(
+          isScrollControlled: true,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          context: context, builder: (context){
+          return const AddExerciseBottomSheet();
+        });
+      },
+      child: const Icon(Icons.add),
+      ),
+      body: const ExerciseViewBody(),
+    );
+  }
+}
+
+class AddExerciseBottomSheet extends StatelessWidget {
+  const AddExerciseBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+class ExerciseViewBody extends StatelessWidget {
+  const ExerciseViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
