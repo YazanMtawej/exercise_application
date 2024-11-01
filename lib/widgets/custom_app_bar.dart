19762f313ef352a62985ea32e57f4e379d0de2d7
Yazan Mtawej <yznmtwj@gmail.com>
2024-11-01 17:05:28 +0300

    add exercise view body and add custom app bar and add custom search icon

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:exercise_application/widgets/custom_search_icon.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key, required this.title, required this.icon, this.onPressed});
+final String title;
+  final IconData icon;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontSize: 28,color: Colors.white),
+        ),
+        const Spacer(),
+        CustomSearchIcon(icon: icon,
+        onPressed: onPressed ,),
+      ],
+    );
+  }
+}
--- lib/widgets/exercise_view_body.dart
+++ lib/widgets/exercise_view_body.dart
@@ -1,8 +1,41 @@
 
+import 'package:exercise_application/cubit/exercise_cubit/exercise_cubit.dart';
+import 'package:exercise_application/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ExerciseViewBody extends StatelessWidget {
+class ExerciseViewBody extends StatefulWidget {
   const ExerciseViewBody({super.key});
+
+  @override
+  State<ExerciseViewBody> createState() => _ExerciseViewBodyState();
+}
+
+class _ExerciseViewBodyState extends State<ExerciseViewBody> {
+  @override
+ void initState(){
+    BlocProvider.of<ExerciseCubit>(context).fetchAlExercise();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+      child: Column(
+        children: [
+          CustomAppBar(
+            title: 'Notes App',
+            icon: Icons.search,
+          ),
+          Expanded(child: NotesListView()),
+        ],
+      ),
+    );
+  }
+}
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
 
   @override
   Widget build(BuildContext context) {
